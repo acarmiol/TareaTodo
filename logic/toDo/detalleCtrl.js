e9adbf8edf0039aa7ab9bbe6bad1bfda7158b201
Natalie Mata <Natalie Mata>
2016-02-07 21:52:24 -0600

    Vista del detalle lista

--- logic/toDo/detalleCtrl.js
+++ logic/toDo/detalleCtrl.js
@@ -1,2 +1,10 @@
 angular.module('toDoList.controllers')
-	.controller("detalleCtrl", [])
+	.controller("detalleCtrl", ['$scope', 'BDService', '$routeParams', function ($scope, BDService, $routeParams) {
+		
+		$scope.toDoList= BDService.getAll();
+		$scope.route= $routeParams.id;
+
+		console.log($scope.toDoList);
+		console.log($scope.route);
+
+	}]);
