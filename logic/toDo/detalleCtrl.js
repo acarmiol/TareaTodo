165c1213ac074c4e4bf55d1198fba08fae227046
Natalie Mata <Natalie Mata>
2016-02-15 00:44:53 -0600

    borrar las actividades terminadas

--- logic/toDo/detalleCtrl.js
+++ logic/toDo/detalleCtrl.js
@@ -1,13 +1,23 @@
 angular.module('toDoList.controllers')
 	.controller("detalleCtrl", ['$scope', 'BDService', '$routeParams', function ($scope, BDService, $routeParams) {
-		
+		var geToDoList=  function () {
+			return BDService.getAll();
+		};
+
 		$scope.toDoList= BDService.getAll();
 		$scope.route= $routeParams.id;
 
 		$scope.clearToDo= function (id) {
 			BDService.clearToDo(id);
-			BDService.getAll();
+			$scope.toDoList= BDService.getAll();
+			$scope.init();
 		};
+
+		$scope.init= function () {
+			$scope.toDos= geToDoList();
+		}
+
+		$scope.init();
 
 		console.log($scope.toDoList);
 		console.log($scope.route);
